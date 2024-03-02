4ddc24e8f762a4d63db4bfaa575ea5711a4c6448
Steven <programmingxtester@gmail.com>
2024-03-02 12:56:26 +0100

    Change colors, hide newspaper, update information, change map to correct location

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const map = L.map("map").setView([25.66516, -100.264651], 16);
+const map = L.map("map").setView([25.711996141653927, -100.2934482337229], 16);
 
 L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -8,4 +8,4 @@ L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-const marker = L.marker([25.66516, -100.264651]).addTo(map);
+const marker = L.marker([25.711996141653927, -100.2934482337229]).addTo(map);
