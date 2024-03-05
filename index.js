1f09f44ac0dd8651c0c2aa539b0fa1d4bcc5116e
Steven <programmingxtester@gmail.com>
2024-03-05 19:54:28 +0100

    Clean-up html document

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 "use strict";
 
-const map = L.map("map").setView([25.711996141653927, -100.2934482337229], 16);
+const displayMap = function () {
+  const map = L.map("map").setView(
+    [25.711996141653927, -100.2934482337229],
+    16
+  );
 
-L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+  L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-const marker = L.marker([25.711996141653927, -100.2934482337229]).addTo(map);
+  const marker = L.marker([25.711996141653927, -100.2934482337229]).addTo(map);
+};
+
+displayMap();
