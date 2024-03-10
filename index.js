504386eef9f763f62823d0c4b96e0352a7f607d8
Steven <steven.aguirrerangel@gmail.com>
2024-03-10 17:43:15 +0100

    Add eventlistener to form

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const form = document.getElementById("form");
+
 const displayMap = function () {
   const map = L.map("map").setView(
     [25.711996141653927, -100.2934482337229],
@@ -16,3 +18,21 @@ const displayMap = function () {
 };
 
 displayMap();
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const businessName = document.getElementById("business").value;
+  const businessEmail = document.getElementById("email").value;
+  const phoneNumber = document.getElementById("phone").value;
+  const contactMsg = document.getElementById("message").value;
+  console.log(
+    firstName,
+    lastName,
+    businessName,
+    businessEmail,
+    phoneNumber,
+    contactMsg
+  );
+});
